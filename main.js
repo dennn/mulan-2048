e00a37da3d9cbd548fb802772f158715d7544ed3
Denis Ogun <do1303@my.bristol.ac.uk>
2014-04-18 01:50:31 +0100

    Integrated 2048 Restart button not yet working

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const IP = "127.0.0.1"
+const IP = "127.0.0.1";
 const PORT = 8080;
 
 // Enum of requests
@@ -8,76 +8,194 @@ var requestType = {
 	GameData : 2
 };
 
+var buttonState = {
+	Released : 0,
+	Pressed : 1
+};
+
+var action = {
+	up : 0,
+	down: 1,
+	left: 2, 
+	right: 3,
+	restart: 4
+}
+
 var socketId;
+var numberOfPlayers = 0;
+var handshaken = false;
 
 chrome.sockets.tcp.create({}, function(createInfo) {
 	socketId = createInfo.socketId;
-  	chrome.sockets.tcp.connect(createInfo.socketId, IP, PORT, onConnectedCallback);
+	chrome.sockets.tcp.connect(createInfo.socketId, IP, PORT, onConnectedCallback);
 });
 
 function onConnectedCallback() {
 	console.log("Connected");
 
 	chrome.sockets.tcp.onReceiveError.addListener(function(info) {
-    	console.log("Error: ", info);
- 	});
+		console.log("Error: ", info);
+	});
 
- 	sendHandshake();
+	sendHandshake();
 
- 	chrome.sockets.tcp.setPaused(info.clientSocketId, false);
+	chrome.sockets.tcp.setPaused(info.clientSocketId, false);
 }
+
+chrome.sockets.tcp.onReceive.addListener(function(info) {
+	if (info.socketId != socketId)
+		return;
+	// info.data is an arrayBuffer.
+	arrayBuffer2String(info.data, function(string) {
+		var jsonObject = JSON.parse(string);
+		if (handshaken === true) {
+			handleGameDataResponse(jsonObject);
+		} else {
+			handleHandshakeResponse(jsonObject);
+		}
+	});
+});
+
+
+/* Responses */
 
 function sendHandshake() {
 	var handshake = {
-  		Request_type: requestType.Handshake,
-  		maxPlayers: 1,
-  		minPlayers: 1,
-  		Buttons: {
-  			Width: 2,
-  			Height: 2
-  		},
-  		Media: [{
-  			Path: "ASKOASK",
-  			Type: 0
-  		}]
-	}
+		Request_type: requestType.Handshake,
+		maxPlayers: 1,
+		minPlayers: 1,
+		Buttons: {
+			Width: 3,
+			Height: 2, 
+			4: {
+				"Title": "Up",
+				"X": 0,
+				"Y": 0
+			},
+			5: {
+				"Title": "Down",
+				"X": 1,
+				"Y": 0
+			},
+			6: {
+				"Title": "Left",
+				"X": 0,
+				"Y": 1
+			},
+			7: {
+				"Title": "Right",
+				"X": 1,
+				"Y": 1
+			},
+			8: {
+				"Title": "Restart",
+				"X": 2,
+				"Y": 0
+			}
+
+		},
+		Media: [{
+		}]
+	};
 
 	var json = JSON.stringify(handshake);
-	console.log(json);
 
 	string2ArrayBuffer(json, function(buf) {
 		chrome.sockets.tcp.send(socketId, buf, function(info) {
-			if (info.resultCode != 0) {
-      			console.log("Send failed.");
+			if (info.resultCode !== 0) {
+				console.log("Send failed.");
 			}
 		});
 	});
 }
 
-chrome.sockets.tcp.onReceive.addListener(function(info) {
-	console.log("Received data")
-  	if (info.socketId != socketId)
-    	return;
-  	// info.data is an arrayBuffer.
-  	console.log(info.data);
-});
+function handleHandshakeResponse(JSON)
+{
+	numberOfPlayers = JSON.Active_Players;
+	handshaken = true;
+	sendGameData();
+}
+
+function sendGameData()
+{
+	var gameData = {
+		Request_type: requestType.GameData,
+		Devices: [4, 5, 6, 7, 8],
+		Player_actions: {}
+	};
+
+	var json = JSON.stringify(gameData);
+
+	string2ArrayBuffer(json, function(buf) {
+		chrome.sockets.tcp.send(socketId, buf, function(info) {
+			if (info.resultCode !== 0) {
+				console.log("Send failed.");
+			}
+		});
+	});
+}
+
+function handleGameDataResponse(JSON)
+{
+	var players = JSON.Player_inputs;
+
+	if (players[0] != null) {
+		var playersDevices = players[0].Devices;
+		if (playersDevices != null) {
+			for (var key in playersDevices) {
+				if (playersDevices.hasOwnProperty(key)) {
+					if (playersDevices[key].Status == 1) {
+						var gesture;
+						console.log("button press")
+						switch (key) {
+							case "4":
+								gesture = action.up;
+								break;
+
+							case "5":
+								gesture = action.down;
+								break;
+
+							case "6":
+								gesture = action.left;
+								break;
+
+							case "7":
+								gesture = action.right;
+								break;
+
+							case "8":
+								gesture = action.restart;
+								break;
+						}
+
+						console.log(gesture);
+						var event = new CustomEvent('mulan-gesture', { 'detail': gesture }); 
+						document.dispatchEvent(event);
+					}
+				}
+			}
+		}
+	}
+	setTimeout(sendGameData, 25);
+}
 
 /* Turn the JSON strings into an array buffer. Chrome sockets require the data to be in this format to send */
 
 function string2ArrayBuffer(string, callback) {
-    var blob = new Blob([string]);
-    var f = new FileReader();
-    f.onload = function(e) {
-        callback(e.target.result);
-    }
-    f.readAsArrayBuffer(blob);
+	var blob = new Blob([string]);
+	var f = new FileReader();
+	f.onload = function(e) {
+		callback(e.target.result);
+	};
+	f.readAsArrayBuffer(blob);
 }
 
 function arrayBuffer2String(buf, callback) {
-    var blob = new Blob([buf]);
-    var f = new FileReader();
-    f.onload = function(e) {
-        callback(e.target.result)
-    }
-    f.readAsText(blob);
+	var blob = new Blob([buf]);
+	var f = new FileReader();
+	f.onload = function(e) {
+		callback(e.target.result);
+	};
+	f.readAsText(blob);
 }
